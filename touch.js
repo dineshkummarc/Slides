c2d91181c325384a02cc3c13a015a2063688d86e
Polikis <contacto.mail99@gmail.com>
2011-11-19 19:26:13 +0100

    Small Fix

--- touch.js
+++ touch.js
@@ -3,17 +3,17 @@ document.addEventListener("touchstart",function(event){
 	event.preventDefault();
 	if(event.target.id == "container" && event.touches.length < 2){
 		startX = event.touches[0].pageX;
+		console.log(startX);
 	}
 },false);
 
 document.addEventListener("touchmove",function(event){
 	event.preventDefault();
-	//console.log(event.target);
 	if(event.target.id == "container" && event.touches.length < 2){
 		//console.log((startX - event.touches[0].pageX))
 		if((startX - event.touches[0].pageX) > 100){
 			previousSlide();
-		} else if((startX - event.touches[0].pageX) < 0){
+		} else if((startX - event.touches[0].pageX) < -100){
 			nextSlide();
 		}
 	}

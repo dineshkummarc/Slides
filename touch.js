36ef16562d7a9b947bb6800a01be5f9e37f67bc9
Polikis <contacto.mail99@gmail.com>
2011-11-19 18:59:24 +0100

    Made the animation quicker on iPad by removing box-shadow. iOS specific css fixed

--- touch.js
+++ touch.js
@@ -1,14 +1,16 @@
 var startX;
 document.addEventListener("touchstart",function(event){
 	event.preventDefault();
-	if(event.target.children[0].className == "slide" && event.touches.length < 2){
+	if(event.target.id == "container" && event.touches.length < 2){
 		startX = event.touches[0].pageX;
 	}
 },false);
 
 document.addEventListener("touchmove",function(event){
 	event.preventDefault();
-	if(event.target.children[0].className == "slide" && event.touches.length < 2){
+	//console.log(event.target);
+	if(event.target.id == "container" && event.touches.length < 2){
+		//console.log((startX - event.touches[0].pageX))
 		if((startX - event.touches[0].pageX) > 100){
 			previousSlide();
 		} else if((startX - event.touches[0].pageX) < 0){

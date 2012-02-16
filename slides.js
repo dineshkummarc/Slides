f9b3cdbd4779c4425f9ffb6eace2eb2a24ed37fe
Polikis <contacto.mail99@gmail.com>
2012-02-16 19:00:54 +0100

    centered Slides Vertically and made UI changes on iPad
    the Slides are now bigger on iPad and a small Bug related to Event
    Handling on iPad

--- slides.js
+++ slides.js
@@ -67,14 +67,14 @@ function setEvent(slide){
 var startX;
 document.addEventListener("touchstart",function(event){
 	event.preventDefault();
-	if(event.touches.length < 2 && !!event.touches[0].target.className.match("current")){
+	if(event.touches.length < 2 /*&& !!event.touches[0].target.className.match("current")*/){
 		startX = event.touches[0].pageX;
 	}
 },false);
 
 document.addEventListener("touchmove",function(event){
 	event.preventDefault();
-	if(event.touches.length < 2 && !!event.touches[0].target.className.match("current")){
+	if(event.touches.length < 2 /*&& !!event.touches[0].target.className.match("current")*/){
 		if((startX - event.touches[0].pageX) > 0){
 			previousSlide();
 		} else if((startX - event.touches[0].pageX) < 0){
